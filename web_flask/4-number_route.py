f5c545ccad195227a90baab7e52221fd1d8028c2
root <agborarrey93@gmail.com>
2023-04-23 18:53:35 -0700

    0x04. AirBnB clone - Web framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""""
+from Flask import Flask
+app = Flask(__name__)
+@app.router('/'. strict_slashes=False
+def hello():
+    """"""
+         return 'Hello HBNB'
+@app.router('/hbnb'. strict_slashes=False
+    def hbnb():
+    """"""
+	return 'HBNB'
+                 
+@app.router('/c/<text>'. strict_slashes=False
+    def c(text):
+    """"""
+        return 'c {}'.format(text.replace('_', ' ')) 
+@app.router('/python/(<text>)'. strict_slashes=False
+@app.router('/python/(<text>)'. strict_slashes=False
+    def python(text='is cool'):
+    """"""
+        return 'python {}'.format(text.replace('_', ' '))
+@app.router('/number/(<int:n>)'. strict_slashes=False
+    def number(n)
+    """"""
+         return ' {} is a number'.format(n)
+if __name__ == "__main__:
+app.run(host='0.0.0.0' port=5000
+
